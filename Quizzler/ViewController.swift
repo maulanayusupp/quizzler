38d475e7ba4256e1a27465da7f3783a1024d739c
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-04-22 17:42:01 +0700

    Quiz and alert

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,7 +12,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank();
+    var pickedAnswer : Bool = false;
+    var questionNumber : Int = 0;
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -22,11 +24,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[questionNumber];
+        questionLabel.text = firstQuestion.questionText;
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if (sender.tag == 1) {
+            pickedAnswer = true;
+        } else if (sender.tag == 2) {
+            pickedAnswer = false;
+        }
+        
+        checkAnswer()
+        questionNumber = questionNumber + 1;
+        nextQuestion()
     }
     
     
@@ -36,19 +48,37 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
+        if (questionNumber < allQuestions.list.count - 1) {
+            questionLabel.text = allQuestions.list[questionNumber].questionText;
+        } else {
+
+            let alert = UIAlertController(title: "Awesone", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in
+                self.startOver();
+            })
+            
+            alert.addAction(restartAction);
+            
+            present(alert, animated: true, completion: nil);
+        }
         
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer;
         
+        if (correctAnswer == pickedAnswer) {
+            print("You got it!");
+        } else {
+            print("Wrong");
+        }
     }
     
     
     func startOver() {
-       
+        print("Victory")
+        questionNumber = 0;
     }
-    
-
-    
 }
